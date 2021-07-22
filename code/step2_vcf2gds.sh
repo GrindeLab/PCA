4aa957c449c90ea9a2575d662246a7d9a491e2a7
Kelsey Grinde <kgrinde@macalester.edu>
2021-07-22 12:32:45 -0500

    remove email

--- code/step2_vcf2gds.sh
+++ code/step2_vcf2gds.sh
@@ -2,8 +2,7 @@
 ## relies on code from TOPMed Analysis Pipeline
 
 pipeline=$1
-email=$2
-config=$3
+config=$2
 
-python ${pipeline}/vcf2gds.py --cluster_file ${pipeline}/cluster_bstudents_cfg.json -e $email -c 1-22 $config
+python ${pipeline}/vcf2gds.py --cluster_file ${pipeline}/cluster_bstudents_cfg.json -c 1-22 $config
 
